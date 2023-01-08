59f4bf408bc364d95d3a879eecbabd7979fe70c8
plwebse <peter.lindblom@plweb.se>
2023-01-08 22:28:04 +0100

    better recorvery if network goes down

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import socket
 import time
 from bmp280 import BMP280SPI
 import secret
+import rp2
 
 spi1_sck = Pin(10)
 spi1_tx = Pin(11)
@@ -12,48 +13,53 @@ spi1_rx = Pin(12)
 spi1_csn = Pin(13, Pin.OUT, value=1)
 spi1 = SPI(1, sck=spi1_sck, mosi=spi1_tx, miso=spi1_rx)
 bmp280_spi = BMP280SPI(spi1, spi1_csn)
+rp2.country('SE')
 
-# gobal settings
+
+
 cache_ttl = 10
-ok_http_header = 'HTTP/1.1 200 OK\r\nContent-type: application/json; charset=utf-8\r\n\r\n'
-not_found_http_header = 'HTTP/1.1 404 Not Found\r\nContent-type: application/json; charset=utf-8\r\n\r\n'
-
+max_wait = 30
 
 wlan = None
-s = None
-addr = None
+server_socket = None
+ip_addr = None
 
 def connect_to_wlan(secret):
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(secret.ssid, secret.password)
+    
+    count = 0
+    while wlan.status() != 3:
+        count = count + 1
+        print('waiting for connection... try:', count)
+        print('ssid:', secret.ssid)
+        print('status:', wlan.status())
+        if count >= max_wait:
+            time.sleep(10)
+            count = 0
+        else:
+            time.sleep(1)
 
-    max_wait = 30
-    while max_wait > 0:
-        if wlan.status() < 0 or wlan.status() >= 3:
-            break
-        max_wait -= 1
-        print('waiting for connection...')
-        time.sleep(1)
-    if wlan.status() != 3:
-        raise RuntimeError('network connection failed')
-    else:
-        print('connected')
-        status = wlan.ifconfig()
-        print( 'ip = ' + status[0] )
+    print('connected to:', secret.ssid)
+    status = wlan.ifconfig()
+    print('ip = ' + status[0] )
     return wlan
 
-def get_addr_info():
-    return socket.getaddrinfo('0.0.0.0', 80)[0][-1]
-
-def create_socket(addr):
-    s = socket.socket()
-    s.settimeout(1.0)
-    s.bind(addr)
-    s.listen(1)
-    print('listening on', addr)
+def create_socket(s):
+    ip_addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+    print('create socket: ', ip_addr)
+    print('socket: ', s)
+    while s is None:
+        s = socket.socket()
+        s.settimeout(1.0)
+        s.bind(ip_addr)
+        s.listen(1)
+        time.sleep(5)
+        print('socket_created:', s)
+    
+    print('listening on', ip_addr)
     return s
-
 
 def send_http_header_and_body_and_close(cl, header,  body):
     cl.send(header)
@@ -61,46 +67,68 @@ def send_http_header_and_body_and_close(cl, header,  body):
     cl.close()
 
 def get_read_out_and_convert_to_json():
+    print('get_read_out_and_convert_to_json')
     readout = bmp280_spi.measurements
     temperature = readout['t']
-    pressure = readout['p']    
+    pressure = readout['p']
     return f'{{"temperature":{temperature}, "temperature-unit":"°C", "pressure":{pressure}, "pressure-unit":"hPa."}}'
- 
-http_body = get_read_out_and_convert_to_json()
-cl = None
-last_fetch = time.time()
-while True:    
-    try:
-        if wlan is None:
-            wlan = connect_to_wlan(secret)
-        
-        if addr is None:
-            addr = get_addr_info()
-        
-        if s is None:
-            s = create_socket(addr)
-        
-        now = time.time()
-        cl, addr = s.accept()
-        print('client connected from', addr)
-        request = cl.recv(1024)
-        request = str(request)
-        try:
-            request = request.split()[1]
-        except IndexError:
-            cl.close()
-                
-        if request == '/':
-            if (now - last_fetch) > cache_ttl:                            
-                http_body = get_read_out_and_convert_to_json()
-                last_fetch = now
-                print('updated:', now)
-            else:
-                print('cached last_fetch was:', last_fetch)
-            send_http_header_and_body_and_close(cl, ok_http_header, http_body)
-        else:
-            send_http_header_and_body_and_close(cl, not_found_http_header, '{}')
 
+def http_request_response(cl, now, last_fetch, cache_ttl, http_body):
+    request = cl.recv(1024)
+    request = str(request)
+    last_update = last_fetch
+    ok_http_header = 'HTTP/1.1 200 OK\r\nContent-type: application/json; charset=utf-8\r\n\r\n'
+    not_found_http_header = 'HTTP/1.1 404 Not Found\r\nContent-type: application/json; charset=utf-8\r\n\r\n'
+    
+    try:
+        request = request.split()[1]
+    except IndexError:
+        cl.close()
+                
+    if request == '/':
+        if (now - last_update) > cache_ttl:                            
+            http_body = get_read_out_and_convert_to_json()
+            last_update = now
+            print('updated:', last_update)
+        else:
+            print('cached last_update was:', last_update)
+        send_http_header_and_body_and_close(cl, ok_http_header, http_body)
+    else:
+        print('404')
+        send_http_header_and_body_and_close(cl, not_found_http_header, '{}')
+    return (http_body, last_update)
+
+last_fetch = time.time()
+http_body = get_read_out_and_convert_to_json()
+
+while True:
+    cl = None
+    now = time.time()
+    try:
+        if wlan is not None and not wlan.isconnected():
+            print(f'not connected to wifi waiting: {max_wait} before tring to connect agian.')
+            time.sleep(max_wait)
+            wlan = None            
+            server_socket = None
+            print("trying to connect to wifi agian")
+        
+        if wlan is None:
+            print('wlan is None')
+            wlan = connect_to_wlan(secret)
+            server_socket = create_socket(server_socket)
+
+        if server_socket is None:
+            print('server_socket is None')
+            server_socket = create_socket(server_socket)
+            time.sleep(10)
+            continue
+
+        cl, cl_ip_addr = server_socket.accept()
+        print('client connected from', cl_ip_addr)
+        http_body, last_fetch = http_request_response(cl, now, last_fetch, cache_ttl, http_body)
+        
     except OSError as e:
         if cl is not None:
             cl.close()
+    except RuntimeError as e:
+        print('runtime error:', e)
